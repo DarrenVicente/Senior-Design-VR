1afd7231097cc8abd6e75181b8aae276ffe83735
DarrenVicente <36481379+DarrenVicente@users.noreply.github.com>
2018-09-09 09:37:58 -0400

    Import of Test File

---
+++ Assets/environment/Follow Camera.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCamera : MonoBehaviour {
+    public Transform Player;
+    private Vector3 offset;
+
+    private bool lookAt = true;
+	// Use this for initialization
+	void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = Player.TransformPoint(offset);
+        if(lookAt){
+            transform.LookAt(Player);
+        }
+	}
+}
